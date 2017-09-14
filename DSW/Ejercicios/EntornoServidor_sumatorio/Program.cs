af79df10be82fd25bd2bc2565345119681dd837a
Aymediacoan Mauleon <yaconeill.amq@gmail.com>
2017-09-14 19:45:53 +0100

    Adding DSW folder

---
+++ DSW/Ejercicios/EntornoServidor_sumatorio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntornoServidor_sumatorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Ist = new List<int>();
+            Ist.Add(1); Ist.Add(2); Ist.Add(3);
+            int r = 0;
+            foreach (var i in Ist)
+            {
+                r = r + i;
+            }
+            Console.WriteLine("Hola el resultado es: "+ r);
+            Console.ReadLine();
+        }
+    }
+}
